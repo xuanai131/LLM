ff182602fff6fb2fcc0067bba087742568d7c4e4
xuanai131 <xuanai297@gmail.com>
2024-05-03 00:44:40 +0700

    remove unnecessary files and fix absolute path

--- Bot/test_tool.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-import time
-transcriber = pipeline("automatic-speech-recognition", model="vinai/PhoWhisper-small")
-start_time  = time.time()
-output = transcriber("/var/www/html/flask/recorded_audio.mp3")['text']
-end_time = time.time()
-print(output ,  end_time-start_time)
