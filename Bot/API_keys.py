677ddd1f1b5ee8c806a831ba058963b87aea1069
xuanai131 <81255180+xuanai131@users.noreply.github.com>
2024-05-03 00:29:13 +0700

    Update API_keys.py

--- Bot/API_keys.py
+++ Bot/API_keys.py
@@ -1,6 +1,1 @@
-import os
 
-os.environ["GOOGLE_CSE_ID"] = "20614afd15a864b79"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBH2eZM0858fxs6G-nuFeV5D1XiU5lr_60"
-os.environ["TAVILY_API_KEY"] = "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"
-os.environ['OPENAI_API_KEY'] = "sk-proj-EorxN1ifGPBC0PSjUFHqT3BlbkFJIy04RIqRRGqLl6urYJts"
