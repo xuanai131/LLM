cf2ddb376eaf3a4d3c3d1cef6477699c1fa8446e
hovandang <hodangtl@gmail.com>
2024-05-03 13:55:42 +0700

    dang change audio

--- flask/subaudio.py
+++ flask/subaudio.py
@@ -2,7 +2,7 @@ import subprocess
 
 # Replace 'script.sh' with the path to your shell script
 def run():
-    script_path = '/var/www/html/flask/audio.sh'
+    script_path = './audio.sh'
 
     # Execute the shell script
     subprocess.run(['bash', script_path])
