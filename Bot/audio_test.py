e584e548c46d6d60e709481a1de354e8fb99200f
xuanai131 <xuanai297@gmail.com>
2024-05-12 13:38:33 +0700

    fix methods's errors and add feature checking exist barcodes and books 2

--- Bot/audio_test.py
+++
@@ -1,8 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-import time
-
-pipe = pipeline("automatic-speech-recognition", model="vinai/PhoWhisper-large")
-
-output = pipe('recorded_audio.mp3')['text']
-print(output)
--- Bot/draw_graph.py
+++
@@ -1,8 +0,0 @@
-#!/home/dang/miniconda3/envs/nlp-task/bin/python
-import Helper_Utilities
-from IPython.display import Image
-
-graph = Helper_Utilities.CreateGraph([])
-image = Image(graph.get_graph().draw_png())
-with open("graph.png", "wb") as f:
-    f.write(graph.get_graph().draw_png())
