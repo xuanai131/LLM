018b1d6d3f8055fecd32e6da0fa0670d521eb1f5
xuanai131 <xuanai297@gmail.com>
2024-05-18 10:10:08 +0700

    Enhance voice assistant feature

--- Bot/retrieval-text/voice_assitant.py
+++ Bot/retrieval-text/voice_assitant.py
@@ -12,172 +12,60 @@ from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 import sys
 sys.path.append("../")
 import API_keys
+import Helper_Utilities
+from Voice_handle import VoiceHandle
+from colorama import Fore, Back, Style
 from openai import OpenAI
 client = OpenAI()
 i = 0
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-function_def = {
-    "name": "route",
-    "description": "trả lời câu hỏi của người dùng",
-    "parameters": {
-        "title": "routeSchema",
-        "type": "object",
-        "properties": {
-            "messages": {
-                "title": "messages",
-                "ouput": 'text',
-            }
-        },
-        "required": ["messages"],
-    },
-}
-system_assistant_prompt = (
-    "Bạn là một trợ lí robot thông minh phục vụ trong thư viện HCMUTE, bạn tên là Librarios. Bạn là một robot di động có 2 bánh xe giúp bạn có thể đi lại tự do trong thư viện, "
-    "nhờ đó bạn có thể dẫn người dùng đến nơi để những cuốn sách mà sinh viên hay giảng viên mong muốn."
-    "Lưu ý: chỉ trả lời những câu hỏi bằng tiếng việt và có nghĩa, nếu không hãy phản hồi là bạn không hiểu câu hỏi."
-)
+OpenAIHistoryConversation = []
+graph = Helper_Utilities.CreateGraph(OpenAIHistoryConversation)
 
-assistant_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_assistant_prompt),
-        # MessagesPlaceholder(variable_name="history"),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-
-assistant_chain = (
-    assistant_prompt
-    | llm.bind_functions(functions=[function_def], function_call="route")
-    | JsonOutputFunctionsParser()
-)
-
-
-
-sleep_timer = None
-wake_word = 'porcupine'
-listening_for_wake_word = True
-responding_to_user = False
-
-def start_sleep_timer():
-    global sleep_timer, listening_for_wake_word
-    if sleep_timer:
-        sleep_timer.cancel()
-    sleep_timer = threading.Timer(10, enter_sleep_mode)
-    sleep_timer.start()
-def reset_sleep_timer():
-    global sleep_timer
-    if sleep_timer:
-        sleep_timer.cancel()
-        sleep_timer = None
-def enter_sleep_mode():
-    global listening_for_wake_word
-    listening_for_wake_word = True
-    print(f"Entered sleep mode. Say the {wake_word} to activate.")
-
-def listen_for_wake_word(audio):
-    # print('/////////////////////////', len(audio.frames))
-    global listening_for_wake_word, i
-    with open('wake_detect.wav', 'wb') as f:
-        f.write(audio.get_wav_data())
-    f = sf.SoundFile('wake_detect.wav')
-    if f.frames > 100:
-        # i += 1
-        # with open(f'audio/wake_detect_{i}.wav', 'wb') as f:
-        #     f.write(audio.get_wav_data())
-        audio_file= open('wake_detect.wav', "rb")
-        transcription = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio_file
-        )
-        text_input = transcription.text
-        print('/////////////////////////////////', text_input)
-        if wake_word in text_input.lower().strip():
-            print('___Assistant: Tôi có thể giúp gì cho bạn?')
-            speak('Tôi có thể giúp gì cho bạn')
-            # speak('Listening')
-            listening_for_wake_word = False
-def play_wav(file_path):
-    data, fs = sf.read(file_path, dtype='float32')
-    sd.play(data, fs)
-    sd.wait()
-def reset_audio(file):
-    with wave.open(file, 'rb') as input_wav:
-        params = input_wav.getparams()
-        empty_audio_data = b''
-        with wave.open(file, 'wb') as output_wav:
-            output_wav.setparams(params)
-            output_wav.writeframes(empty_audio_data)
-def speak(text):
-    global responding_to_user
-    print('set responding_to_user to True')
-    responding_to_user = True
-    stop_recognizer()
-    response = client.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=text,
-    )
-
-    response.write_to_file("speak.mp3")
-    play_wav("speak.mp3")
-    reset_audio('wake_detect.wav')
-    reset_audio('prompt.wav')
-    start_recognizer()
-    print('set responding_to_user to False')
-    responding_to_user = False
-def GPTResponse(audio):
-    global listening_for_wake_word
-    try:
-        with open('prompt.wav', 'wb') as f:
-            f.write(audio.get_wav_data())
-        f = sf.SoundFile('prompt.wav')
-        if f.frames > 100:
-            audio_file= open('prompt.wav', "rb")
-            transcription = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=audio_file
-            )
-            prompt_text = transcription.text
-            if len(prompt_text.strip()) == 0:
-                print('Empty prompt. Please speak again.')
-                speak('Empty prompt. Please speak again.')
-                listening_for_wake_word = True
-            else:
-                print('_____User: ' + prompt_text)
-                output = assistant_chain.invoke([HumanMessage(prompt_text)])['messages'][0]
-                print('___Assistant: ', output)
-                speak(output)
-    except Exception as e:
-        print('Prompt error: ', e)
-def callback(recognizer, audio):
-    global listening_for_wake_word, responding_to_user
-    if listening_for_wake_word:
-        listen_for_wake_word(audio)
+def handle_event_response(attitude,answer):
+    global OpenAIHistoryConversation,redirect_state
+    print("check history: ",OpenAIHistoryConversation)
+    if (attitude == "bad"):
+        print(Fore.RED +"in the bad response")
+        print(Style.RESET_ALL)
+        redirect_state = "Book_researcher"
+        Helper_Utilities.write_state(redirect_state)
+        inputs = {
+        "messages": OpenAIHistoryConversation
+    }
+        res  = run_graph(inputs)
+        return res
     else:
-        if not responding_to_user:
-            reset_sleep_timer()
-            GPTResponse(audio)
-            start_sleep_timer()
-source, r = None, None
-def stop_recognizer():
-    global source, stop_listening
-    stop_listening(wait_for_stop=False)
-    # source.__exit__(None, None, None)
-def start_recognizer():
-    global source, r,stop_listening
-    source = sr.Microphone()
-    with source as s:
-        r.adjust_for_ambient_noise(s, duration=1)
-    stop_listening = r.listen_in_background(source, callback)
+        redirect_state = "supervisor"
+        Helper_Utilities.write_state(redirect_state)
+        return answer
+def run_graph(inputs):
+    for s in graph.stream(inputs):
+        if "__end__" not in s:
+            print(s)
+            print('------------------------')
+        else:
+            print(s)
+            try:
+                answer = AIMessage(s.get('__end__')['messages'][-1].content)
+                print(answer)
+                attitude = s.get('__end__')['inspector']
+                print("the inspector check that the response is ",attitude)
+                res = handle_event_response(attitude,answer)
+                return res
+            except:
+                pass
+def get_Chat_response(text:str) -> str:
+    print('...Generating answer...')
+    query = HumanMessage(text)
+    OpenAIHistoryConversation.append(query)
+    inputs = {
+        # "history" : [],
+        "messages": OpenAIHistoryConversation
+    }
+    answer = run_graph(inputs)
+    OpenAIHistoryConversation.append(answer)
+    return answer.content
 
-
-
-r = sr.Recognizer()
-source = sr.Microphone()
-with source as s:
-    r.adjust_for_ambient_noise(s, duration=1)
-stop_listening = r.listen_in_background(source, callback)
-print('\nSay', wake_word, 'to wake me up. \n')
-while True:
-    time.sleep(1)
+voicehandle = VoiceHandle(wake_words=['porcupine'], get_chat_response_func=get_Chat_response)    
+voicehandle.run()
