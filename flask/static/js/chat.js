0a6530f0db2b046a512a32e55f2f0811c6ed5cc0
Nguyenhuynhlamvu <ronin792002@gmail.com>
2024-05-20 14:51:53 +0700

    gui update 20-5-2024

--- flask/static/js/chat.js
+++ flask/static/js/chat.js
@@ -2,7 +2,11 @@
     
 $(document).ready(function() {
     $("#messageArea").on("submit",  function(event) {
-        handleEvent(event,"chat", "",check_tool_running);
+
+        handleEvent(event, "chat", "", check_tool_running);
+        // console.log("click send button");
+
+        // handleEvent(event,"chat", "",check_tool_running);
     });
 });
 // const socket2 = io();
@@ -13,55 +17,58 @@ $(document).ready(function() {
 // });
 function handleEvent(event,action,message,tool_running){
     const date = new Date();
-        const hour = date.getHours();
-        const minute = date.getMinutes();
-        const str_time = hour+":"+minute;
-        if (action == "chat"){
-            var rawText = $("#text").val();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const str_time = hour+":"+minute;
+    if (action == "chat"){
+        var rawText = $("#text").val();
+    }
+    if (action == "voice")
+        var rawText = message;
+    if (action == "button"){
+        var rawText = message;
+    }
+    var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
+    $("#text").val("");
+    $("#messageFormeight").append(userHtml);
+    if (tool_running == false)
+    {$.ajax({
+        data: {
+            msg: rawText,
+        },
+        type: "POST",
+        url: "/get",
+        beforeSend: function() {
+            // Add your function or code here to execute while waiting for the response
+            console.log("Waiting for response...");
+            setTimeout(function() {
+                // Add botHtml to #messageFormeight after the delay
+                add_loading_text();
+            }, 1000);
+            
+            // You can add loading animations or other UI updates here
         }
-        if (action == "voice")
-            var rawText = message;
-        var userHtml = '<div class="d-flex justify-content-end mb-4"><div class="msg_cotainer_send">' + rawText + '<span class="msg_time_send">'+ str_time + '</span></div><div class="img_cont_msg"><img src="https://i.ibb.co/d5b84Xw/Untitled-design.png" class="rounded-circle user_img_msg"></div></div>';
-        $("#text").val("");
-        $("#messageFormeight").append(userHtml);
-        if (tool_running == false)
-        {$.ajax({
+    }).done(function(data) {
+        console.log("Request succeeded!");
+        // console.log("image: ", images);
+        // console.log("Response data:", data);
+        if ($("#messageFormeight").children().last().find('span').length === 0) {
+            $("#messageFormeight").children().last().remove();
+        }
+        
+        add_bot_message(data,str_time);
+        // Create a container for the animation
+    });}
+    else {
+        $.ajax({
+            url: "/user_input_mess",
+            type: "POST",
             data: {
                 msg: rawText,
             },
-            type: "POST",
-            url: "/get",
-            beforeSend: function() {
-                // Add your function or code here to execute while waiting for the response
-                console.log("Waiting for response...");
-                setTimeout(function() {
-                    // Add botHtml to #messageFormeight after the delay
-                    add_loading_text();
-                }, 1000);
-                
-                // You can add loading animations or other UI updates here
-            }
-        }).done(function(data) {
-            console.log("Request succeeded!");
-            // console.log("image: ", images);
-            // console.log("Response data:", data);
-            if ($("#messageFormeight").children().last().find('span').length === 0) {
-                $("#messageFormeight").children().last().remove();
-            }
-            
-            add_bot_message(data,str_time);
-            // Create a container for the animation
-        });}
-        else {
-            $.ajax({
-                url: "/user_input_mess",
-                type: "POST",
-                data: {
-                    msg: rawText,
-                },
-            });
-        }
-        if (action == "chat") event.preventDefault();
+        });
+    }
+    if (action == "chat") event.preventDefault();
 }
 function scrollToBottom() {
     var messageFormeight = document.getElementById("messageFormeight");
